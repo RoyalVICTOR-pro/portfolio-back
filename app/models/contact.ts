95abc0c4d5eb3f6a3b50f06423f3392bc9650a47
Royal VICTOR <tech@steewo.io>
2024-12-12 10:09:04 +0100

    first commit

---
+++ app/models/contact.ts
@@ -0,0 +1,22 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class Contact extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare name: string
+
+  @column()
+  declare email: string
+
+  @column()
+  declare phone: string
+
+  @column()
+  declare message: string
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+}
